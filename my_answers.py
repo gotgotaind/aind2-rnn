703dee2bb69de9a2c68f9f6af7f0c6dd7b65cf48
gotgotaind <gotgotaind@gmail.com>
2017-07-09 22:55:14 +0200

    project submitted for review

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Activation
 import keras
 import string
 from collections import defaultdict
@@ -29,7 +30,7 @@ def window_transform_series(series,window_size):
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(step_size, window_size):
+def build_part1_RNN(window_size):
     model = Sequential()
     model.add(LSTM(5, input_shape= (window_size,1)))
     model.add(Dense(1))
@@ -37,23 +38,24 @@ def build_part1_RNN(step_size, window_size):
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
-def clean_text(text):
+def cleaned_text(text):
     # remove as many non-english characters and character sequences as you can 
     # but don't remove the punctuation given below
     punctuation = ['!', ',', '.', ':', ';', '?']
 
-    valid_chars=list(string.ascii_letters)+list(string.digits)+list(punctuation)
+    cleaned_text=text
+    valid_chars=list(string.ascii_letters)+list(punctuation)
 
     # build a dict of all chars in the text
     chars = defaultdict(int)
-    for char in text:
+    for char in cleaned_text:
         chars[char]=chars[char]+1
 
     for char in chars:
         if char not in valid_chars:
-            text=text.replace(char,' ')
+            cleaned_text=cleaned_text.replace(char,' ')
 
-    return text
+    return cleaned_text
 
 
 
@@ -62,11 +64,27 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    i=0
+    while i*step_size+window_size<len(text):
+        #print("i {}, i*step_size+window_size {}, len text {}".format(i,i*step_size+window_size,len(text)))
+        inputs.append(text[i*step_size:i*step_size+window_size])
+        outputs.append(text[i*step_size+window_size])
+        i=i+1
     
   
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
-def build_part2_RNN(step_size, window_size):
-    pass
+def build_part2_RNN(window_size,len_chars):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,len_chars) ))
+    model.add(Dense(len_chars))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    return model
+
+
+
